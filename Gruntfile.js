35919844c96134ea5b270597594ff290b1dd0104
alawatthe <alawatthe@googlemail.com>
2014-05-19 18:32:56 +0200

    removed some browsers which apparently aren't supported right now by saucelabs

--- Gruntfile.js
+++ Gruntfile.js
@@ -497,7 +497,7 @@ module.exports = function (grunt) {
 					concurrency: 3,
 					detailedError: true,
 					passed: true,
-					build: 76,
+					build: 77,
 					testReadyTimeout: 10000,
 					testname: 'MathLib QUnit test suite',
 					tags: ['MathLib', 'v<%= pkg.version %>'],
